dd8f58bb9c7955e7f0d30f11be240fb1a3cab232
Dave Turner <davetii@gmail.com>
2018-10-12 21:44:59 -0400

    Basic components example - Componentizing VisibleToggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,60 @@
 'use strict';
 
-var showDetails = false;
-function handleDetailsView() {
-    showDetails = !showDetails;
-    render();
-};
-var appRoot = document.getElementById('app');
-var render = function render() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle'
-        ),
-        React.createElement(
-            'button',
-            { onClick: handleDetailsView },
-            showDetails ? 'Hide Details' : 'Show Details'
-        ),
-        showDetails && React.createElement(
-            'div',
-            null,
-            'I am the details'
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-render();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var VisibleToggle = function (_React$Component) {
+    _inherits(VisibleToggle, _React$Component);
+
+    function VisibleToggle(props) {
+        _classCallCheck(this, VisibleToggle);
+
+        var _this = _possibleConstructorReturn(this, (VisibleToggle.__proto__ || Object.getPrototypeOf(VisibleToggle)).call(this, props));
+
+        _this.handleDetailsView = _this.handleDetailsView.bind(_this);
+        _this.state = { show: false };
+        return _this;
+    }
+
+    _createClass(VisibleToggle, [{
+        key: 'handleDetailsView',
+        value: function handleDetailsView() {
+            this.setState(function (currentState) {
+                return { show: !currentState.show };
+            });
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Visibility Toggle'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.handleDetailsView },
+                    this.state.show ? 'Hide Details' : 'Show the fung Details'
+                ),
+                this.state.show && React.createElement(
+                    'div',
+                    null,
+                    'I am the details'
+                )
+            );
+        }
+    }]);
+
+    return VisibleToggle;
+}(React.Component);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(React.createElement(VisibleToggle, null), appRoot);
--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,18 +1,25 @@
-let showDetails = false;
-function handleDetailsView() {
-    showDetails = !showDetails;
-    render();
-};
+class VisibleToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleDetailsView = this.handleDetailsView.bind(this);
+        this.state =  { show: false };
+    }
+
+    handleDetailsView() {
+        this.setState((currentState) => {
+            return {show : !currentState.show}
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.handleDetailsView}>{this.state.show ? 'Hide Details' : 'Show the fung Details'}</button>
+                {this.state.show && ( <div>I am the details</div> )}
+            </div>
+            )
+    }
+}
 const appRoot = document.getElementById('app');
-const render = () => {
-    const template =
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={handleDetailsView}>{showDetails ? 'Hide Details' : 'Show Details'}</button>
-            {showDetails && ( <div>I am the details</div> )}
-        </div>
-    ReactDOM.render(template, appRoot);
-};
-
-render();
-
+ReactDOM.render(<VisibleToggle />, appRoot);
