c353f4545cbfb6c99938978c424f5563bc1be159
Dave Turner <davetii@gmail.com>
2018-10-13 16:53:07 -0400

    Assignments with default export

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ console.log('utils.js is running');
 
 const square = (n) =>  n *n ;
 const add = (n1, n2) =>  n1 + n2;
-
+const subtract = (n1, n2) =>  n1 - n2;
 export {
-    square, add
+    square, add, subtract as default
 }
