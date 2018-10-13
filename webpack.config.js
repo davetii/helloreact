cda197bcc3d3fbde1d2934daa32e41ddb64695f9
Dave Turner <davetii@gmail.com>
2018-10-13 16:22:00 -0400

    Added Webpack funcitonality

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+// entry -> output
+
+module.exports = {
+    entry : './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+}
