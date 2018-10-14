9301124baf3e564aa960f1b9a0ae07b3491f8740
Dave Turner <davetii@gmail.com>
2018-10-13 21:05:45 -0400

    Configure project for webpack and babel

--- src/app.js
+++ src/app.js
@@ -1,21 +1,6 @@
-//import './utils.js';
-import subtract, {square, add} from './utils.js';
-import isSenior,  {isAdult, canDrink} from './Person.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-
-console.log('app js is running');
-console.log('the square root of 4 is : ' + square(4));
-console.log('4 added to 4 is  : ' + add(4, 4));
-console.log('3 subtracted from 12 is   : ' + subtract(12, 3));
-
-console.log('Is a 17 year old an adult : ' + isAdult(17));
-console.log('Is a 18 year old an adult : ' + isAdult(18));
-console.log('Is a 22 year old an adult : ' + isAdult(22));
-console.log('Is a 21 year old an adult : ' + isAdult(21));
-
-console.log('Can a 20 year old drink? : ' + canDrink(20));
-console.log('Can a 21 year old drink? : ' + canDrink(21));
-console.log('Can a 22 year old drink? : ' + canDrink(22));
-
-console.log('Is a 22 year old a senior? : ' + isSenior(22));
-console.log('Is a 66 year old a senior? : ' + isSenior(66));
+const appRoot = document.getElementById('app');
+const template = <h1>This is JSX from WebPack</h1>
+ReactDOM.render(template,appRoot);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require('path');
 
-// entry -> output
-
 module.exports = {
     entry : './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
-}
+};
